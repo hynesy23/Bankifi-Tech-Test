3b7109dd52398ae94ae707fea06a98e1701dbd0f
Cillian <hynesy23@hotmail.com>
2019-12-18 17:36:51 +0000

    Marvel experience server error so rendering an error msg for my users

---
+++ src/components/MainPageError.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function MainPageError() {
+  return (
+    <h1 className="main_err">
+      This site is currently experiencing some problems connecting to the Marvel
+      Developer Portal. We are working on the problem and hope to have it up and
+      running again in no time. Thank you for your understanding.
+    </h1>
+  );
+}
